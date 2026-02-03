99f9c993e0bccf97086dd757530f0bb8c74ec9ea
Micah Pawlicki <mich.pawlicki@gmail.com>
2026-02-03 13:43:58 -0600

    client queue creation

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	connString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connString)
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer conn.Close()
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("Something went wrong logging in: %v", err)
+	}
+	pubsub.DeclareAndBind(conn, "peril_direct", "pause."+username, "pause", pubsub.QueueTransient)
+	//waiting for ctrl+c
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt)
+	<-signalCh
+	fmt.Println("Interrupt detected, shutting down...")
 }
---
+++ internal/pubsub/pubsub.go
@@ -0,0 +1,82 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	bytes, err := json.Marshal(val)
+	if err != nil {
+		log.Fatalf("Error marshalling JSON: %v", err)
+	}
+	pub := amqp.Publishing{
+		ContentType: "application/json",
+		Body:        bytes,
+	}
+	err = ch.PublishWithContext(context.Background(), exchange, key, false, false, pub)
+	if err != nil {
+		log.Fatalf("Error publishing to channel: %v", err)
+	}
+	return nil
+}
+
+type SimpleQueueType int
+
+const (
+	QueueDurable   SimpleQueueType = iota //0
+	QueueTransient                        // 1
+)
+
+var queueType = map[SimpleQueueType]string{
+	QueueDurable:   "durable",
+	QueueTransient: "transient",
+}
+
+func (qt SimpleQueueType) String() string {
+	return queueType[qt]
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType, // an enum to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Error creating channel: %v", err)
+	}
+
+	var durable bool
+	var autoDelete bool
+	var exclusive bool
+
+	switch queueType {
+	case QueueDurable:
+		durable = true
+		autoDelete = false
+		exclusive = false
+	case QueueTransient:
+		durable = false
+		autoDelete = true
+		exclusive = true
+	}
+
+	q, err := ch.QueueDeclare(queueName, durable, autoDelete, exclusive, false, nil)
+	if err != nil {
+		log.Fatalf("Error declaring queue: %v", err)
+	}
+
+	err = ch.QueueBind(queueName, key, exchange, false, nil)
+	if err != nil {
+		log.Fatalf("Error binding queue: %v", err)
+	}
+
+	return ch, q, nil
+}
