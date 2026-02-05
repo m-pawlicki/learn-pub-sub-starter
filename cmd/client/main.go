ad6265536c17a226d29574c5b696a560afd7ad41
Micah Pawlicki <mich.pawlicki@gmail.com>
2026-02-05 17:18:02 -0600

    client publishing moves

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -19,20 +19,45 @@ func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
 	}
 }
 
+func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
+	return func(mv gamelogic.ArmyMove) {
+		defer fmt.Print("> ")
+		gs.HandleMove(mv)
+	}
+}
+
 func main() {
 	fmt.Println("Starting Peril client...")
+
 	connString := "amqp://guest:guest@localhost:5672/"
+
 	conn, err := amqp.Dial(connString)
 	if err != nil {
 		log.Fatalf("Could not connect: %v", err)
 	}
 	defer conn.Close()
+
+	publishCh, err := conn.Channel()
+	if err != nil {
+		log.Fatalf("Could not create channel: %v", err)
+	}
+	defer publishCh.Close()
+
 	username, err := gamelogic.ClientWelcome()
 	if err != nil {
 		log.Fatalf("Something went wrong logging in: %v", err)
 	}
+
 	gs := gamelogic.NewGameState(username)
-	pubsub.SubscribeJSON(conn, routing.ExchangePerilDirect, "pause."+username, routing.PauseKey, pubsub.QueueTransient, handlerPause(gs))
+
+	var (
+		userPause = "pause." + username
+		userMoves = "army_moves." + username
+	)
+
+	pubsub.SubscribeJSON(conn, routing.ExchangePerilDirect, userPause, routing.PauseKey, pubsub.QueueTransient, handlerPause(gs))
+	pubsub.SubscribeJSON(conn, routing.ExchangePerilTopic, userMoves, "army_moves.*", pubsub.QueueTransient, handlerMove(gs))
+
 client_loop:
 	for {
 		words := gamelogic.GetInput()
@@ -43,10 +68,16 @@ client_loop:
 		case "spawn":
 			gs.CommandSpawn(words)
 		case "move":
-			_, err := gs.CommandMove(words)
-			if err == nil {
-				fmt.Println("Move successful.")
+			mv, err := gs.CommandMove(words)
+			if err != nil {
+				fmt.Println("Move failed.")
 			}
+			err = pubsub.PublishJSON(publishCh, routing.ExchangePerilTopic, userMoves, mv)
+			if err != nil {
+				fmt.Printf("Error publishing move: %v", err)
+				continue
+			}
+			fmt.Println("Move published successfully.")
 		case "status":
 			gs.CommandStatus()
 		case "help":
